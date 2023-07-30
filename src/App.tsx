2a0e006e3833f8354cdc4bfc7dee24a43ddec42d
Ivan Marchuk <imarchuk90@gmail.com>
2023-07-30 18:24:28 +0300

    Updates

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Card } from './components/Card';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+
+        <Card />
+    </div>
+  );
+}
+export default App;
