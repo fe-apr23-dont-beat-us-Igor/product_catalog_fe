f54e9150a3250aa3a58b8735aefe5a9345a42772
Ivan Marchuk <imarchuk90@gmail.com>
2023-07-31 18:24:22 +0300

    Implement LikeButton logic

--- src/components/UI/LikeButton.tsx
+++ src/components/UI/LikeButton.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import classNames from 'classnames';
+import { FC, ButtonHTMLAttributes } from 'react';
 
-const LikeButton = ({...atributes}) => {
-  return <button className="likeButton likeButton--selected" {...atributes}></button>;
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  // implement your logic in parent component
+  selected: boolean;
+}
+
+const LikeButton: FC<Props> = ({ selected, ...atributes }) => {
+  const className = classNames('likeButton', {
+    'likeButton--selected': selected,
+  });
+
+  return <button className={className} {...atributes}></button>;
 };
 
 export default LikeButton;
