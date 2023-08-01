61d3e5bbcacb8bd12672bae7040201a1b2f0efe8
Ivan Marchuk <imarchuk90@gmail.com>
2023-08-01 20:34:13 +0300

    Add pagination UI

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,66 +3,26 @@ import { useSearchParams, Link } from 'react-router-dom';
 import { visiblePageLinks } from '../utils/visiblePageLinks';
 import { getNewSearchParams } from '../utils/getNewSearchParams';
 import './Pagination.scss';
+import PuginationButton from '../UI/PuginationButton';
+import SliderButton from '../UI/SliderButton';
 
 type Props = {
-  total: number,
+  total: number;
 };
 
-export const Pagination: React.FC<Props> = ({ total }) => {
-  const [searchParams] = useSearchParams();
-  const perPage = searchParams.get('perPage') || null;
-  const page = searchParams.get('page') || '1';
-  const numberOfPages = Math.ceil(total / (Number(perPage) || total));
- 
-  const linkToChange = (page: string | null) => {
-    return {
-      search: getNewSearchParams (searchParams, { 
-        page, 
-      }),
-    };
-  };
-
-  return numberOfPages > 1 ? (
-      <div className="pagination__container">
-        <div className="pagination__item">
-          <Link 
-            to={linkToChange(+page === 1 
-              ? null 
-              : `${+page - 1}`)}
-            className={page === '1' 
-              ? "arrow__left__disabled" 
-              : "arrow__left" }
-          />
-        </div>
-        
-        <ul className="pagination">
-          {visiblePageLinks(numberOfPages, page).map((pageLink) => (
-            <li className="pagination__item">
-              <Link 
-                to={linkToChange(pageLink === 1 
-                  ? null 
-                  : pageLink.toString())}
-                className={+page === pageLink 
-                  ? "page__link__selected"
-                  : "page__link"}
-              >
-                {pageLink}
-              </Link>
-             </li>  
-          ))}
-          
-        </ul>
-
-        <div className="pagination__item">
-        <Link 
-            to={linkToChange(+page === numberOfPages 
-              ? page 
-              : `${+page + 1}`)}
-            className={+page === numberOfPages
-              ? "arrow__right__disabled" 
-              : "arrow__right" }
-          />
-        </div>
+const Pagination = () => {
+  return (
+    <div className="pagination">
+      <SliderButton left={true} />
+      <div className="pagination__buttons">
+        <PuginationButton pageNumber={1} selected={true} />
+        <PuginationButton pageNumber={2} selected={false} />
+        <PuginationButton pageNumber={3} selected={false} />
+        <PuginationButton pageNumber={4} selected={false} />
+      </div>
+      <SliderButton left={false} />
     </div>
-  ) : null;
+  );
 };
+
+export default Pagination;
