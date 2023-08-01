e0ab0bd8fb76ab30ed6dc53973c8383131b89f36
Sonya Liubchenko <conilub@gmail.com>
2023-08-01 18:26:26 +0300

    fixed not found page

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -4,6 +4,11 @@ import { img } from '../../images/images';
 export const NotFoundPage: FC = () => {
   return (
     <section className="not-found section">
+      <img 
+        alt='not-found-404'
+        className='not-found__pic'
+        src={img.notFound}
+      />
       <article className="not-found__info">
         <h1 className="not-found__info--h1">Sorry,</h1>
         <h4 className="not-found__info--h4">Please check URL and try again.</h4>
@@ -14,11 +19,6 @@ export const NotFoundPage: FC = () => {
           Back to Home
         </a>
       </article>
-      <img 
-        alt='not-found-404'
-        className='not-found__pic'
-        src={img.notFound}
-      />
     </section>
   );
 };
