03f1906a359c35b3bd1752ea043dd00f5a2f8280
Mager <magerazp@gmail.com>
2023-08-04 16:53:14 +0300

    Remade to map method

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -5,25 +5,63 @@ import { img } from "../../images/images";
 
 export const Breadcrumbs: React.FC = () => {
   const location = useLocation();
-  console.log(location);
+  const paths = location.pathname.split('/').filter(path => path !== '');
+  const searchParams = location.search.toString() || '';
+  console.log(location, paths, location.search);
+
+  let secondLinkTitle = '';
+  switch (searchParams) {
+    case '?category=tablets':
+      secondLinkTitle = 'TABLETS';
+      break;
+
+    case '?category=accessories':
+      secondLinkTitle = 'ACCESSORIES';
+      break;
+
+    default:
+      secondLinkTitle = 'PHONES';
+  };
 
   return (
-    <div className="breadscrumbs">
-      <Link to="/home">
-        <img
-          src={img.home}
-          alt="home-icon"
-          className="breadcrumbs__home-icon"
-        />
-      </Link>
-      <img
-        src={img.arrowRight}
-        alt="arrow-icon"
-        className="breadcrumbs__arrow"
-      />
-      <Link to="/phones">
-        <span className="breadcrumbs__text">Phones</span>
-      </Link>
-    </div>
+    <ul className="breadcrumbs">
+      <li className="breadcrumbs__item">
+        <Link to="/home">
+          <img
+            src={img.home}
+            alt="home-icon"
+            className="breadcrumbs__home-icon"
+          />
+        </Link>
+      </li>
+      {paths.map((path, index) => {
+        const separator = (
+          <img
+            src={img.arrowRight}
+            alt="arrow-icon"
+            className="breadcrumbs__arrow"
+          />
+        );
+        if (index === paths.length - 1) {
+          return (
+            <li key={index} className="breadcrumbs__item">
+              {separator}
+              <span>
+                {path}
+              </span>
+            </li>
+          );
+        }
+
+          return (
+            <li key={index} className="breadcrumbs__item">
+              {separator}
+              <Link to={'/products' + searchParams}>
+                {secondLinkTitle}
+              </Link>
+            </li>
+          );
+      })}
+    </ul>
   );
 };
