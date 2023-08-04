4f4ecada575e5e375c6bcbf2a4a4af1ea0f53ffa
Ivan Marchuk <imarchuk90@gmail.com>
2023-08-04 14:34:25 +0300

    add UseLocalStorage hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import './styles/index.scss';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Outlet } from 'react-router-dom';
-import { BurgerMenu } from './components/BurgerMenu/BurgerMenu';
+
 
 const App: FC = () => {
+
   return (
     <div className="App">
       <Header />
--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -7,6 +7,7 @@ import Pagination from '../Pagination/Pagination';
 
 import { getSomeProducts } from '../../api/api';
 import { ProductCollection } from '../../Types/products.types';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 export const SortingOpgions = ['Newest', 'Oldest', 'Prise'];
 export const PaginationOptions: string[] = ['16', '32', '64'];
