e80107bcf0124d0815b517e015049846d12f989e
Ivan Marchuk <imarchuk90@gmail.com>
2023-08-08 10:36:12 +0300

    remove cart context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import { FC } from 'react';
 import GoodsSliderCollection from '../components/GoodsSliderCollection/GoodsSliderCollection';
 import Categories from '../components/Categories/Categories';
-
-import Slider from '../components/slider/Slider';
 import { useProductsAPI } from '../hooks/useFetch';
 import { Product, ProductCollection } from '../Types/products.types';
 import { getNewProducts } from '../api/api';
+import Slider from '../components/Slider/Slider';
 
 const Home: FC = () => {
   const [newProducts, loading, error] = useProductsAPI<ProductCollection>(
