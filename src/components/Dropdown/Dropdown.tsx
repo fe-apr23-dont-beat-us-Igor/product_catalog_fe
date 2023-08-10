77648160718c9fe70df448ba8cf881d76244c397
Ivan Marchuk <imarchuk90@gmail.com>
2023-08-11 01:37:40 +0300

    fix Dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,8 +1,8 @@
 import { FC, MouseEventHandler, useState, SelectHTMLAttributes } from 'react';
 import classnames from 'classnames';
-import { Link, useSearchParams } from 'react-router-dom';
 import { SearchLink } from '../SearchLink';
 import { SortOption } from '../Catalog/Catalog';
+import { useClickOutside } from '../../hooks/useClickOutside';
 
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string;
@@ -13,6 +13,7 @@ export const Dropdown: FC<Props> = ({ label, options }) => {
   const [currentOption, setcurrentOption] = useState<SortOption>(options[0]);
   const [isActive, setIsActive] = useState<boolean>(false);
   const [title] = currentOption;
+  const dropdownRef = useClickOutside<any>(setIsActive);
 
   const handleOptionChange = (event: any, option: SortOption) => {
     event.preventDefault();
@@ -26,7 +27,7 @@ export const Dropdown: FC<Props> = ({ label, options }) => {
   };
 
   return (
-    <div style={{ display: 'inline-block' }}>
+    <div style={{ display: 'inline-block' }} ref={dropdownRef}>
       <p className="small-text dropdown__label">{label}</p>
       <article
         className={classnames('dropdown', {
