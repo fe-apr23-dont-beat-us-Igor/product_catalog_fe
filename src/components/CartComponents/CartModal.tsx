848d09fa99d4f51a14a2280cafbb8577fd6ba766
Ivan Marchuk <imarchuk90@gmail.com>
2023-08-11 08:36:11 +0300

    fix cart

--- src/components/CartComponents/CartModal.tsx
+++ src/components/CartComponents/CartModal.tsx
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react';
-// import '../../styles/components/CartModal.scss';
+import classNames from 'classnames';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 type Props = {
-    handleModal: () => void,
-}
+  handleModal: () => void;
+  isModalVisible: boolean;
+  setIsModalVisible: (value: boolean) => void;
+};
 
-export const CartModal: React.FC<Props> = ({ handleModal }) => {  
+export const CartModal: React.FC<Props> = ({ handleModal, isModalVisible }) => {
   return (
-    <div className='modal__container'>
-      <div className='modal__content'>
-        <h2 className='modal__text'>Thank you for your purchase!</h2>
-        <div className='modal__line'></div>
+    <div
+      className={classNames('modal__container', {
+        'modal__container--hidden': !isModalVisible,
+        'modal__container--active': isModalVisible,
+      })}
+    >
+      <div className="modal__content">
+        <h2 className="modal__text">Thank you for your purchase!</h2>
+        <div className="modal__line"></div>
         <div className="modal__buttons_container">
-          <NavLink
-            to="/home"
-          >
+          <NavLink to="/home">
             <button className="modal__button" onClick={handleModal}>
               Return to main page
             </button>
